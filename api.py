4e101e5072afa84f474727d57611b53ffbc6bc62
JuanFryS <jfsalca@gmail.com>
2015-04-27 18:20:35 +0200

    Trabajando en cargar imagenes siguientes

--- api.py
+++ api.py
@@ -8,12 +8,15 @@ import sys
 
 class instagramRequest(webapp2.RequestHandler):
   def get(self):
+    #endpoint = self.request.get("endpoint", default_value="")
     access_token = self.request.get("access_token", default_value="")
-
-    print("Peticion")
+    max_id = self.request.get("max_id", default_value="")
+    count = self.request.get("count", default_value="")
     
-    respuesta = urllib2.urlopen("https://api.instagram.com/v1/users/self/feed?access_token="+access_token).read()
-
+    if (max_id == ""):
+      respuesta = urllib2.urlopen("https://api.instagram.com/v1/users/self/feed?access_token="+access_token).read()
+    else:
+      respuesta = urllib2.urlopen("https://api.instagram.com/v1/users/self/feed?access_token="+access_token+"&max_id="+max_id+"&count="+count).read()
     self.response.write(respuesta)
     
 app = webapp2.WSGIApplication([
