9abec5dd2512f9ee55287889487aad34d3e60ea6
JuanFryS <jfsalca@gmail.com>
2015-04-28 13:32:26 +0200

    Añadido refresco y mejorado estilo boton cargar mas

--- api.py
+++ api.py
@@ -8,14 +8,25 @@ import sys
 
 class instagramRequest(webapp2.RequestHandler):
   def get(self):
-    peticion = self.request.get("peticion", default_value="")
     access_token = self.request.get("access_token", default_value="")
     count = self.request.get("count", default_value="")
-    if (peticion == ""):
-      respuesta = urllib2.urlopen("https://api.instagram.com/v1/users/self/feed?access_token="+access_token).read()
+    min_id = self.request.get("min_id", default_value="")
+    max_id = self.request.get("max_id", default_value="")
 
+    peticion = "https://api.instagram.com/v1/users/self/feed?access_token="+access_token
+
+    #Peticion basica
+    if (count == "" and min_id == "" and max_id == ""):
+      print ("Peticion basica")
+      respuesta = urllib2.urlopen(peticion).read()
+    #Recargar datos
+    elif (min_id != ""):
+      print ("Peticion refresco")
+      respuesta = urllib2.urlopen(peticion+"&min_id="+min_id).read()
+    #Cargar mas datos
     else:
-      respuesta = urllib2.urlopen(peticion+"&count="+count).read()
+      print ("Peticion cargar mas")
+      respuesta = urllib2.urlopen(peticion+"&max_id="+max_id+"&count="+count).read()
     self.response.write(respuesta)
     
 app = webapp2.WSGIApplication([
