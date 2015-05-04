48717ee85a77691ce25aaf8b42769446e6a29742
JuanFryS <jfsalca@gmail.com>
2015-05-04 16:28:28 +0200

    Commit final

--- api.py
+++ api.py
@@ -3,7 +3,7 @@
 
 
 import webapp2
-import urllib2
+import urllib2, httplib
 import sys
 
 class instagramRequest(webapp2.RequestHandler):
@@ -12,23 +12,25 @@ class instagramRequest(webapp2.RequestHandler):
     count = self.request.get("count", default_value="")
     min_id = self.request.get("min_id", default_value="")
     max_id = self.request.get("max_id", default_value="")
+#    media_id = self.request.get("media_id", default_value="")
 
+#    print(media_id)
     peticion = "https://api.instagram.com/v1/users/self/feed?access_token="+access_token
 
     #Peticion basica
     if (count == "" and min_id == "" and max_id == ""):
-      print ("Peticion basica")
       respuesta = urllib2.urlopen(peticion).read()
     #Recargar datos
     elif (min_id != ""):
-      print ("Peticion refresco")
       respuesta = urllib2.urlopen(peticion+"&min_id="+min_id).read()
+#    elif (media_id != ""):
+#      peticion = "https://api.instagram.com/v1"
+#      respuesta = urllib2.urlopen(peticion+"/media/"+media_id+"/likes").read()
     #Cargar mas datos
     else:
-      print ("Peticion cargar mas")
       respuesta = urllib2.urlopen(peticion+"&max_id="+max_id+"&count="+count).read()
     self.response.write(respuesta)
-    
+
 app = webapp2.WSGIApplication([
     (r'/request/instagram', instagramRequest)
 ], debug=True)
