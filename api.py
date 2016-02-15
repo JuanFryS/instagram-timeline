fef22690863f31f34de1990f9bba6722a3081fe1
JuanFryS <jfsalca@gmail.com>
2016-02-15 12:55:34 +0100

    All posts are updated when refresh button is used

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ class instagramRequest(webapp2.RequestHandler):
   def get(self):
     access_token = self.request.get("access_token", default_value="")
     count = self.request.get("count", default_value="")
-    min_id = self.request.get("min_id", default_value="")
+#    min_id = self.request.get("min_id", default_value="")
     max_id = self.request.get("max_id", default_value="")
 #    media_id = self.request.get("media_id", default_value="")
 
@@ -18,11 +18,11 @@ class instagramRequest(webapp2.RequestHandler):
     peticion = "https://api.instagram.com/v1/users/self/feed?access_token="+access_token
 
     #Peticion basica
-    if (count == "" and min_id == "" and max_id == ""):
+    if (count == "" and max_id == ""):
       respuesta = urllib2.urlopen(peticion).read()
     #Recargar datos
-    elif (min_id != ""):
-      respuesta = urllib2.urlopen(peticion+"&min_id="+min_id).read()
+#    elif (min_id != ""):
+#      respuesta = urllib2.urlopen(peticion+"&min_id="+min_id).read()
 #    elif (media_id != ""):
 #      peticion = "https://api.instagram.com/v1"
 #      respuesta = urllib2.urlopen(peticion+"/media/"+media_id+"/likes").read()
